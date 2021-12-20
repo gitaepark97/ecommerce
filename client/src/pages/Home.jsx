7780b5cd09239680e97cb3d3f6c896d11244976c
gitaepark <parkkitae7@gmail.com>
2021-12-20 21:39:40 +0900

    Create Pure React Slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 export default function Home() {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   )
 }
